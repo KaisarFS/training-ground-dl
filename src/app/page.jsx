3fa480c1ee79855819b259ce7e23f805939eb3b1
Kaisar Firjatullah Sani Setyawan <kaisar.10rpl2.2019@gmail.com>
2025-01-15 21:07:56 +0700

    experimenting with the react state behaviour

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,27 @@
 'use client';
 
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function HomePage() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts'
+        );
         setPosts(response.data.slice(0, 10));
       } catch (error) {
-        setError(error)
+        setError(error);
       } finally {
         setLoading(false);
       }
-    }
+    };
 
     fetchPosts();
   }, []);
@@ -27,30 +30,45 @@ export default function HomePage() {
     try {
       setLoading(true);
       await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
-      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id))
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
     } catch (error) {
       setError('Failed to delete a post', error);
       console.error(error);
     } finally {
       setLoading(false);
     }
-  }
+  };
+
+  const handleCount = async () => {
+    console.log('ba')
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+  };
 
   if (loading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
 
   if (error) {
-    return <p>{error}</p>
+    return <p>{error}</p>;
   }
 
   return (
     <div className="p-8">
+      <div>
+        <h1 className="text-white">{count}</h1>
+        <button onClick={handleCount()} className="p-4 bg-red-200 ounded-xl">
+          Count
+        </button>
+      </div>
       <h2 className="text-2xl font-bold mb-4">List Posts</h2>
-      <ul className='space-y-4'>
+      <ul className="space-y-4">
         {posts.map((post, index) => (
-          <li key={post.id} className='p-4 border rounded shadow'>
-            <h3> {index + 1}. {post.title}</h3>
+          <li key={post.id} className="p-4 border rounded shadow">
+            <h3>
+              {' '}
+              {index + 1}. {post.title}
+            </h3>
             <p>{post.body}</p>
 
             <button onClick={() => handleDelete(post.id)}>Delete</button>
